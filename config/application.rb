14853b509b54b3da36c9111269d9690025722b4f
Mikroudz <severi.olsbo@gmail.com>
2017-11-20 12:42:07 +0200

    helsinki as default timezone

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module IotAutonpolttaja
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.after_initialize do
-    config.time_zone "Europe/Helsinki"
+    config.time_zone = 'Europe/Helsinki'
+    config.active_record.default_timezone = :local
+
     end
   end
 end
