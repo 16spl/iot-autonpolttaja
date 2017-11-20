183115c27a3a00f5df1001d17b0775504eb4035c
Mikroudz <severi.olsbo@gmail.com>
2017-11-20 12:36:11 +0200

    fixed timezones

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module IotAutonpolttaja
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.after_initialize do
-
+    config.time_zone "Europe/Helsinki"
     end
   end
 end
