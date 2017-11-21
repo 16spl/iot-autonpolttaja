42f084809b29cd6d42b6130b7bbc19127880315d
Mikroudz <severi.olsbo@gmail.com>
2017-11-21 12:59:17 +0200

    wont accept letters or negative numbers

--- app/models/tempature.rb
+++ app/models/tempature.rb
@@ -1,10 +1,12 @@
 class Tempature < ApplicationRecord
   def self.to_csv(days)
-    if days.blank?
+    if /\A[0-9]+\z/.match(days) and not days.blank?
+      days = days.to_i
+    else
       days = 7
     end
     temp = Tempature.find_by_sql(
-    ["Select * FROM tempatures WHERE id%15 = 0 AND created_at > ? ORDER BY created_at DESC LIMIT 10000", Time.now - days.to_i.days]
+    ["Select * FROM tempatures WHERE id%15 = 0 AND created_at > ? ORDER BY created_at DESC LIMIT 10000", Time.now - days.days]
     )
     csv = ""
     csv << "time,temp\n"
